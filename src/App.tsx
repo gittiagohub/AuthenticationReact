0b631c03e4b3d5c8234f040b10b7e3669cbfe369
Tiago da Silva <tiagoslashaxl@gmail.com>
2024-12-09 21:11:57 -0300

    Iniciando tela de login

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Login from "./Components/Login/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Login></Login>
+      <p>Página de Login</p>
+    </div>
+  );
+}
+
+export default App;
